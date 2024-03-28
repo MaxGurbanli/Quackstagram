e65dee93cecc14ecfb691b3a0a3282e90ebd707f
Max Gurbanli <gurbanlimaksud@gmail.com>
2024-03-28 13:14:51 +0100

    Merge latest code

--- NotificationsUI.java
+++ NotificationsUI.java
@@ -8,7 +8,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 public class NotificationsUI extends JFrame implements Observer {
