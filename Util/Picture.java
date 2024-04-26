770ce3a4ab09829c30e232777c34c4d797c6747c
Max Gurbanli <gurbanlimaksud@gmail.com>
2024-04-26 14:45:39 +0200

    Add initial plantUML setup

--- Util/Picture.java
+++ Util/Picture.java
@@ -1,24 +1,15 @@
 package Util;
-import java.util.List;
-import java.util.ArrayList;
 
 // Represents a picture on Quackstagram
 class Picture {
     private String imagePath;
     private String caption;
     private int likesCount;
-    private List<String> comments;
 
     public Picture(String imagePath, String caption) {
         this.imagePath = imagePath;
         this.caption = caption;
         this.likesCount = 0;
-        this.comments = new ArrayList<>();
-    }
-
-    // Add a comment to the picture
-    public void addComment(String comment) {
-        comments.add(comment);
     }
 
     // Increment likes count
@@ -37,9 +28,5 @@ class Picture {
 
     public int getLikesCount() {
         return likesCount;
-    }
-
-    public List<String> getComments() {
-        return comments;
     }
 }
