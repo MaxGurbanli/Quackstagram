1df38191004dec4a5155a528f2edce9be0abd50b
Max Gurbanli <gurbanlimaksud@gmail.com>
2024-05-06 21:43:13 +0200

    Improve Schema and remove unused triggers

--- SQL/Schema.sql
+++ SQL/Schema.sql
@@ -1,45 +1,45 @@
-CREATE DATABASE IF NOT EXISTS quack;
+DROP DATABASE IF EXISTS quack;
+CREATE DATABASE quack;
 
 USE quack;
 
 CREATE TABLE User (
-  username VARCHAR(255) PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) UNIQUE,
   bio VARCHAR(255),
-  password VARCHAR(255),
-  postsCount INT,
-  followersCount INT,
-  followingCount INT
+  password VARCHAR(255)
 );
 
-CREATE TABLE Post (
+CREATE TABLE Picture (
   id INT AUTO_INCREMENT PRIMARY KEY,
   imagePath VARCHAR(255),
   caption VARCHAR(255),
   likesCount INT,
-  username VARCHAR(255),
-  FOREIGN KEY (username) REFERENCES User(username)
+  author INT,
+  FOREIGN KEY (author) REFERENCES User(id)
 );
 
-CREATE TABLE PostLike (
+CREATE TABLE PictureLike (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(255),
-  postId INT,
-  FOREIGN KEY (username) REFERENCES User(username),
-  FOREIGN KEY (postId) REFERENCES Post(id)
+  likerId INT,
+  pictureId INT,
+  FOREIGN KEY (likerId) REFERENCES User(id),
+  FOREIGN KEY (pictureId) REFERENCES Picture(id)
 );
 
 CREATE TABLE Follow (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(255),
-  target VARCHAR(255),
-  FOREIGN KEY (username) REFERENCES User(username),
-  FOREIGN KEY (target) REFERENCES User(username)
+  followerId INT,
+  targetId INT,
+  FOREIGN KEY (followerId) REFERENCES User(id),
+  FOREIGN KEY (targetId) REFERENCES User(id)
 );
 
 CREATE TABLE Notification (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(255),
-  target VARCHAR(255),
-  FOREIGN KEY (username) REFERENCES User(username),
-  FOREIGN KEY (target) REFERENCES User(username)
+  notifierId INT,
+  targetId INT,
+  type VARCHAR(255),
+  FOREIGN KEY (notifierId) REFERENCES User(id),
+  FOREIGN KEY (targetId) REFERENCES User(id)
 );
--- SQL/Views.sql
+++ SQL/Views.sql
@@ -1,15 +1,15 @@
--- Shows the number of posts each user has liked (user behavior)
+-- Shows the number of pictures each user has liked (user behavior)
 CREATE VIEW UserLikes AS
 SELECT username AS username, COUNT(*) AS totalLikes
-FROM PostLike
+FROM PictureLike
 GROUP BY username
 HAVING totalLikes > 0;
 
--- Shows the number of likes for each post (content popularity)
-CREATE VIEW PopularPosts AS
-SELECT postId AS postID, COUNT(*) AS numberOfLikes
-FROM PostLike
-GROUP BY postID
+-- Shows the number of likes for each picture (content popularity)
+CREATE VIEW PopularPictures AS
+SELECT pictureId AS pictureID, COUNT(*) AS numberOfLikes
+FROM PictureLike
+GROUP BY pictureID
 HAVING numberOfLikes > 0;
 
 -- Shows the number of notifications for each user (system analytics)
@@ -19,5 +19,5 @@ FROM Notification
 GROUP BY target
 HAVING COUNT(*) > 0;
 
-CREATE INDEX ind_postlike_username ON PostLike(username);
+CREATE INDEX ind_picturelike_username ON PictureLike(username);
 CREATE INDEX ind_notification_target ON Notification(target);
