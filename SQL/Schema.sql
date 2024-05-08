c95d046e3e2c4c7f9176519e61ccf179aa8d2de1
Max Gurbanli <gurbanlimaksud@gmail.com>
2024-05-08 16:47:54 +0200

    Improve ERD and Schema

--- SQL/Schema.sql
+++ SQL/Schema.sql
@@ -1,22 +1,24 @@
 DROP DATABASE IF EXISTS quack;
 CREATE DATABASE quack;
-
 USE quack;
 
 CREATE TABLE User (
   id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255) UNIQUE,
   bio VARCHAR(255),
-  password VARCHAR(255)
+  password VARCHAR(255),
+  picturesCount INT DEFAULT 0,
+  followersCount INT DEFAULT 0,
+  followingCount INT DEFAULT 0
 );
 
 CREATE TABLE Picture (
   id INT AUTO_INCREMENT PRIMARY KEY,
   imagePath VARCHAR(255),
   caption VARCHAR(255),
-  likesCount INT,
-  author INT,
-  FOREIGN KEY (author) REFERENCES User(id)
+  likesCount INT DEFAULT 0,
+  authorId INT,
+  FOREIGN KEY (authorId) REFERENCES User(id)
 );
 
 CREATE TABLE PictureLike (
@@ -26,7 +28,6 @@ CREATE TABLE PictureLike (
   FOREIGN KEY (likerId) REFERENCES User(id),
   FOREIGN KEY (pictureId) REFERENCES Picture(id)
 );
-
 CREATE TABLE Follow (
   id INT AUTO_INCREMENT PRIMARY KEY,
   followerId INT,
@@ -34,12 +35,11 @@ CREATE TABLE Follow (
   FOREIGN KEY (followerId) REFERENCES User(id),
   FOREIGN KEY (targetId) REFERENCES User(id)
 );
-
 CREATE TABLE Notification (
   id INT AUTO_INCREMENT PRIMARY KEY,
   notifierId INT,
-  targetId INT,
   type VARCHAR(255),
+  pictureId INT,
   FOREIGN KEY (notifierId) REFERENCES User(id),
-  FOREIGN KEY (targetId) REFERENCES User(id)
+  FOREIGN KEY (pictureId) REFERENCES Picture(id)
 );
