be7edb9a177f0afa7a65b7fad3c83773e58ae786
Max Gurbanli <gurbanlimaksud@gmail.com>
2024-05-17 15:38:31 +0200

    Add drop view statements

--- SQL/Views.sql
+++ SQL/Views.sql
@@ -1,3 +1,4 @@
+DROP VIEW IF EXISTS UserLikes;
 -- Shows the number of pictures each user has liked (user behavior)
 CREATE VIEW UserLikes AS
 SELECT likerId AS likerId, COUNT(*) AS totalLikes
@@ -5,6 +6,7 @@ FROM PictureLike
 GROUP BY likerId
 HAVING totalLikes > 0;
 
+DROP VIEW IF EXISTS PopularPictures;
 -- Shows the number of likes for each picture (content popularity)
 CREATE VIEW PopularPictures AS
 SELECT imagePath AS imagePath, COUNT(*) AS numberOfLikes
@@ -12,6 +14,7 @@ FROM PictureLike
 GROUP BY imagePath
 HAVING numberOfLikes > 0;
 
+DROP VIEW IF EXISTS SystemAnalytics;
 -- Shows the number of notifications for each user (system analytics)
 CREATE VIEW SystemAnalytics AS
 SELECT targetId AS targetId, COUNT(*) AS notifications_count
